3b059b5ccfb19e226c0c0319871f44ca6fc92d7e
Roshini250103 <109164465+Roshini250103@users.noreply.github.com>
2022-09-30 09:12:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ChangeColors from "./ChangeColors";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ChangeColors />
+    </div>
+  );
+}
